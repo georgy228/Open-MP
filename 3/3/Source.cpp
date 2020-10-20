41fd9b5361cb5de06e3ec087f9baf1491fe258bc
sekibura <51335422+sekibura@users.noreply.github.com>
2020-10-20 20:34:48 +0300

    add some

--- 3/3/Source.cpp
+++ 3/3/Source.cpp
@@ -5,105 +5,104 @@
 #include<vector>
 #include<algorithm> 
 using namespace std;
-void Init(vector<size_t>* A);
-void PrintVector(vector<size_t> vec);
-vector<size_t> MaxElement(vector<size_t>* A, vector<size_t>* B);
-//vector<size_t> MaxElementMP(vector<size_t>* A, vector<size_t>* B);
-void MaxElementMP(vector<size_t>* A, vector<size_t>* B);
+void Init(size_t* A, size_t size);
+void PrintArray(size_t*arr, size_t size);
+size_t MaxElement(size_t* A, size_t* B, size_t size);
+size_t MaxElementMP(size_t* A, size_t* B, size_t size);
 void main() {
 
 	srand(time(NULL));
 	
-	size_t size = 50000000;
-	vector<size_t> A(size), B(size), C, C_MP;
+	size_t size = 10,C, C_MP;
+	size_t* A = new size_t[size];
+	size_t * B = new size_t[size];
 
 	double start;
 	double end;
 
 
-	//check threads working--------------------------------------
-	int th_num, num_ths, max_th;
+	//проверка работы потоков--------------------------------------
+	size_t th_num, num_ths, max_th;
 	max_th = omp_get_max_threads();
-	cout << "Max threads=" << max_th << "\n";
+	printf_s("Max threads= %zu\n",max_th) ;
 
 #pragma omp parallel num_threads(max_th) private (num_ths, th_num)
 	{
 		th_num = omp_get_thread_num();
 		num_ths = omp_get_num_threads();
-		printf("I am ready %d from %d \n", th_num, num_ths);
+		printf("I am ready %zu from %zu \n", th_num, num_ths);
 	}
 	//end check-------------------------------------------------
 
-	Init(&A);
-	Init(&B);
-	//PrintVector(A);
-	//PrintVector(B);
+	Init(A,size);
+	Init(B,size);
 
-	start = omp_get_wtime();
-	C = MaxElement(&A, &B);
-	end = omp_get_wtime();
-	//PrintVector(C);
-	printf("Work took %f seconds\n", end - start);
+	start = omp_get_wtime();			// запуск отсчета времени
+	C = MaxElement(A,B,size);		   // запуск функции вычисления максимума (реализация без многопоточности)
+	end = omp_get_wtime();			  // остановка отсчета времени
+	printf_s("C = %zu\n", C);		 // вывод результата функции - сумма элементов массива С, где C[i]=Max(A[i],B[i])
+	printf("Work took %f seconds\n", end - start); // вывод затраченного времени
 
-	start = omp_get_wtime();
-	//C_MP = MaxElementMP(&A, &B);
-	MaxElementMP(&A, &B);
-	end = omp_get_wtime();
-	//PrintVector(C_MP);
-	printf("Work took %f seconds MP\n", end - start);
+
+	start = omp_get_wtime();		   // запуск отсчета времени
+	C_MP = MaxElementMP(A, B,size);   // запуск функции вычисления максимума (реализация c использованием OpenMP)
+	end = omp_get_wtime();			 // остановка отсчета времени
+	printf_s("C_MP = %zu\n", C_MP); // вывод результата функции - сумма элементов массива С, где C[i]=Max(A[i],B[i])
+	printf("Work took %f seconds MP\n", end - start);// вывод затраченного времени
+	
+
 	system("Pause");
 }
 
-void Init(vector<size_t>* A) {
-	for (size_t i = 0; i < (*A).size(); i++)
+// Заполняет массив А размером size случайными числами
+void Init(size_t* A, size_t size) { 
+	for (size_t i = 0; i < size; i++)
 	{
-		(*A).at(i) = (rand() % 100);
+		A[i] = (rand() % 100);
 	}
 }
 
-vector<size_t> MaxElement(vector<size_t>* A, vector<size_t>* B) {
-	vector<size_t> result;
+// Находит C[i]=Max(A[i],B[i]), возвращает сумму элементов С. (Без OpenMP)
+size_t MaxElement(size_t* A, size_t* B,size_t size) {
+	size_t *C=new size_t[size];
 	size_t sum = 0;
-	if ((*A).size() == (*B).size()) {
-		result.resize((*A).size());
+	for (size_t i = 0; i < size; i++)
+	{
+		sum += C[i] = max(A[i], B[i]);
+	}	
+	//PrintArray(C, size);
+	return sum;
+}
 
-		for (size_t i = 0; i < (*A).size(); i++)
-		{
-			sum += result[i] = max((*A)[i], (*B)[i]);
-
-		}
+// Выводит содержимое масиива arr размером size
+void PrintArray(size_t*arr,size_t size) {
+	printf_s("\n");
+	for (size_t i = 0; i < size ; i++)
+	{
+		printf_s("%zu ", arr[i]);
 	}
-	cout << "\nsum = " << sum << "\n";
-	return result;
+	printf_s("\n");
 }
 
-void PrintVector(vector<size_t> vec) {
-	cout << "\n";
-	for (size_t i : vec)
-		cout << i << ' ';
-	cout << "\n";
-}
-
-/*vector<size_t>*/void MaxElementMP(vector<size_t>* A, vector<size_t>* B) {
-	vector<size_t> result;
+// Находит C[i]=Max(A[i],B[i]), возвращает сумму элементов С. (С OpenMP)
+ size_t MaxElementMP(size_t* A, size_t* B,size_t size) {
+	
+	size_t* C = new size_t[size];
 	size_t sum = 0;
 	int i;
-	int max_ = 0;
-	if ((*A).size() == (*B).size()) {
-		//result.resize((*A).size());
-#pragma omp parallel shared(A,B)private(result,max_)
+	size_t max_ = 0;
+	
+
+#pragma omp parallel shared(A,B,C)private(max_)
 		{
 #pragma omp for private(i) reduction(+:sum)
-			for (i = 0; i < (*A).size(); i++)
-			{
-				 /*result[i] = max((*A)[i], (*B)[i]);*/
-				max_ = max((*A)[i], (*B)[i]);
-				result.push_back(max_);				
+			for (i = 0; i < size; i++)
+			{				
+				max_ = max(A[i], B[i]);
+				C[i] = max_;				
 				sum = sum + max_;
 			}
-			//PrintVector(result);
 		}
-	}
-	cout << "\nsum MP = " << sum << "\n";
-	/*return result;*/
+	//PrintArray(C, size);
+	return sum;
 }
