1f178719666ab32cca129c91c0ae2451011263de
sekibura <51335422+sekibura@users.noreply.github.com>
2020-10-26 20:47:35 +0300

    done

--- 4/lab4/Source.cpp
+++ 4/lab4/Source.cpp
@@ -2,109 +2,166 @@
 #include<stdio.h>
 #include<iostream>
 #include<time.h>
-#include<vector>
 #include<algorithm> 
 using namespace std;
-void Init(vector<size_t>* A);
-void PrintVector(vector<size_t> vec);
-vector<size_t> MaxElement(vector<size_t>* A, vector<size_t>* B);
-//vector<size_t> MaxElementMP(vector<size_t>* A, vector<size_t>* B);
-void MaxElementMP(vector<size_t>* A, vector<size_t>* B);
+void Init(size_t* A, size_t size);
+void PrintArray(size_t*arr, size_t size);
+size_t MaxElement(size_t* A, size_t* B, size_t size);
+size_t MaxElementMPAtomic(size_t* A, size_t* B, size_t size);
+size_t MaxElementMPCritical(size_t* A, size_t* B, size_t size);
+size_t MaxElementMPReduction(size_t* A, size_t* B, size_t size);
 void main() {
 
 	srand(time(NULL));
 
-	size_t size = 50000000;
-	vector<size_t> A(size), B(size), C, C_MP;
+	size_t size = 10000000, C, C_MP;
+	size_t* A = new size_t[size];
+	size_t * B = new size_t[size];
 
 	double start;
 	double end;
 
 
-	//check threads working--------------------------------------
-	int th_num, num_ths, max_th;
+	//Check OpenMP working--------------------------------------
+	size_t th_num, num_ths, max_th;
 	max_th = omp_get_max_threads();
-	cout << "Max threads=" << max_th << "\n";
+	printf_s("Max threads= %zu\n", max_th);
 
 #pragma omp parallel num_threads(max_th) private (num_ths, th_num)
 	{
 		th_num = omp_get_thread_num();
 		num_ths = omp_get_num_threads();
-		printf("I am ready %d from %d \n", th_num, num_ths);
+		printf("I am ready %zu from %zu \n", th_num, num_ths);
 	}
 	//end check-------------------------------------------------
 
-	Init(&A);
-	Init(&B);
-	//PrintVector(A);
-	//PrintVector(B);
+	Init(A, size);
+	Init(B, size);
 
-	start = omp_get_wtime();
-	C = MaxElement(&A, &B);
-	end = omp_get_wtime();
-	//PrintVector(C);
-	printf("Work took %f seconds\n", end - start);
+	start = omp_get_wtime();			// start time counter
+	C = MaxElement(A, B, size);		   // start func  (without OpenMP)
+	end = omp_get_wtime();			  // stop time counter
+	printf_s("C = %zu\n", C);		 // print func`s result - summ array`s elements, where C[i]=Max(A[i],B[i])
+	printf("Work took %f seconds\n", end - start); // print time result
 
-	start = omp_get_wtime();
-	//C_MP = MaxElementMP(&A, &B);
-	MaxElementMP(&A, &B);
-	end = omp_get_wtime();
-	//PrintVector(C_MP);
-	printf("Work took %f seconds MP\n", end - start);
+
+	start = omp_get_wtime();		   // start time counter
+	C_MP = MaxElementMPAtomic(A, B, size);   // start func  (with OpenMP Atomic)
+	end = omp_get_wtime();			 // stop time counter
+	printf_s("C_MP Atomic = %zu\n", C_MP); // print func`s result - sum array`s elements, where C[i]=Max(A[i],B[i])
+	printf("Work took %f seconds MP\n", end - start);// print time result
+
+	start = omp_get_wtime();		   // start time counter
+	C_MP = MaxElementMPCritical(A, B, size);   // start func  (with OpenMP Critical)
+	end = omp_get_wtime();			 // stop time counter
+	printf_s("C_MP Critical = %zu\n", C_MP); // print func`s result - sum array`s elements, where C[i]=Max(A[i],B[i])
+	printf("Work took %f seconds MP\n", end - start);// print time result
+
+	start = omp_get_wtime();		   // start time counter
+	C_MP = MaxElementMPReduction(A, B, size);   // start func  (with OpenMP Reduction)
+	end = omp_get_wtime();			 // stop time counter
+	printf_s("C_MP Reduction = %zu\n", C_MP); // print func`s result - sum array`s elements, where C[i]=Max(A[i],B[i])
+	printf("Work took %f seconds MP\n", end - start);// print time result
+
 	system("Pause");
 }
 
-void Init(vector<size_t>* A) {
-	for (size_t i = 0; i < (*A).size(); i++)
+// init A[size]
+void Init(size_t* A, size_t size) {
+	for (size_t i = 0; i < size; i++)
 	{
-		(*A).at(i) = (rand() % 100);
+		A[i] = (rand() % 100);
 	}
 }
 
-vector<size_t> MaxElement(vector<size_t>* A, vector<size_t>* B) {
-	vector<size_t> result;
+// find C[i]=Max(A[i],B[i]), return element`s sum. (without openmp)
+size_t MaxElement(size_t* A, size_t* B, size_t size) {
+	size_t *C = new size_t[size];
 	size_t sum = 0;
-	if ((*A).size() == (*B).size()) {
-		result.resize((*A).size());
-
-		for (size_t i = 0; i < (*A).size(); i++)
-		{
-			sum += result[i] = max((*A)[i], (*B)[i]);
-
-		}
+	for (size_t i = 0; i < size; i++)
+	{
+		sum += C[i] = max(A[i], B[i]);
 	}
-	cout << "\nsum = " << sum << "\n";
-	return result;
+	//PrintArray(C, size);
+	return sum;
 }
 
-void PrintVector(vector<size_t> vec) {
-	cout << "\n";
-	for (size_t i : vec)
-		cout << i << ' ';
-	cout << "\n";
+// print array
+void PrintArray(size_t*arr, size_t size) {
+	printf_s("\n");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf_s("%zu ", arr[i]);
+	}
+	printf_s("\n");
 }
 
-/*vector<size_t>*/void MaxElementMP(vector<size_t>* A, vector<size_t>* B) {
-	vector<size_t> result;
+// find C[i]=Max(A[i],B[i]), return element`s sum. (with openmp)
+size_t MaxElementMPAtomic(size_t* A, size_t* B, size_t size) {
+
+	size_t* C = new size_t[size];
 	size_t sum = 0;
 	int i;
-	int max_ = 0;
-	if ((*A).size() == (*B).size()) {
-		//result.resize((*A).size());
-#pragma omp parallel shared(A,B,sum) private(result,max_)
+	size_t max_ = 0;
+
+
+#pragma omp parallel shared(A,B,C,sum)private(max_)
+	{
+#pragma omp for private(i) /*reduction(+:sum)*/
+		for (i = 0; i < size; i++)
 		{
-#pragma omp for private(i) 
-			for (i = 0; i < (*A).size(); i++)
-			{
-				/*result[i] = max((*A)[i], (*B)[i]);*/
-				max_ = max((*A)[i], (*B)[i]);
-				result.push_back(max_);
-				#pragma omp atomic
-				sum +=max_;
-			}
-			//PrintVector(result);
+			max_ = max(A[i], B[i]);
+			C[i] = max_;
+#pragma omp atomic
+			sum +=max_;
 		}
 	}
-	cout << "\nsum MP = " << sum << "\n";
-	/*return result;*/
+	//PrintArray(C, size);
+	return sum;
+}
+
+size_t MaxElementMPCritical(size_t* A, size_t* B, size_t size) {
+
+	size_t* C = new size_t[size];
+	size_t sum = 0;
+	int i;
+	size_t max_ = 0;
+
+
+#pragma omp parallel shared(A,B,C,sum)private(max_)
+	{
+#pragma omp for private(i) /*reduction(+:sum)*/
+		for (i = 0; i < size; i++)
+		{
+			max_ = max(A[i], B[i]);
+			C[i] = max_;
+#pragma omp critical
+			sum += max_;
+		}
+	}
+	//PrintArray(C, size);
+	return sum;
+}
+
+size_t MaxElementMPReduction(size_t* A, size_t* B, size_t size) {
+
+	size_t* C = new size_t[size];
+	size_t sum = 0;
+	int i;
+	size_t max_ = 0;
+
+
+#pragma omp parallel shared(A,B,C)private(max_)
+	{
+#pragma omp for private(i) reduction(+:sum)
+		for (i = 0; i < size; i++)
+		{
+			max_ = max(A[i], B[i]);
+			C[i] = max_;
+
+			sum += max_;
+		}
+	}
+	//PrintArray(C, size);
+	return sum;
 }
